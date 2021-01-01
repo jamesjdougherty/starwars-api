7e0d8a94523c25508246e96815a7dd66597e492c
James Dougherty <jjdougherty89@gmail.com>
2021-01-01 13:02:34 -0500

    Initial commit

---
+++ src/test/kotlin/com/doughland/starwarsapi/StarwarsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.doughland.starwarsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarwarsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
